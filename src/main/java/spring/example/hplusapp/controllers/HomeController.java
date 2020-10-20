705244429fb14b62a4f0ad0650c359ba1c225e9f
Nihat Allahverdiyev <nihatallahverdiyev@gmail.com>
2020-10-20 17:36:35 +0200

    Added files

---
+++ src/main/java/spring/example/hplusapp/controllers/HomeController.java
@@ -0,0 +1,15 @@
+package spring.example.hplusapp.controllers;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
+public class HomeController {
+
+    @GetMapping("/home")
+    public String goHome() {
+        System.out.println("In the home controller");
+
+        return "index";
+    }
+}
---
+++ src/test/java/spring/example/hplusapp/HplusappApplicationTests.java
@@ -0,0 +1,13 @@
+package spring.example.hplusapp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HplusappApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
